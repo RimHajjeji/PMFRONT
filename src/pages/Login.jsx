8f9ff426a79d6af7d75444245653e15ee6b81ab9
RimHajjeji <rimhajjeji210@gmail.com>
2024-10-09 12:52:21 +0100

    rim

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
         }
     
         try {
-            const res = await axios.post("http://localhost:5000/api/admin/login", { email, password });
+            const res = await axios.post("https://envoices.premiummotorscars.com:5000/api/admin/login", { email, password }); 
             localStorage.setItem("token", res.data.token);
             toast.success("Connexion réussie !");
             navigate("/dashboard"); 
